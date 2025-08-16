c8065e08576518d4b317ba9f6100f710037d87c1
Max Mylnikov <mmmylnikov@yandex.ru>
2025-08-16 19:27:25 +0500

    update test.py

--- test.py
+++ test.py
@@ -1,16 +1,16 @@
-import astdot
+from src import astdot
 
 
 def test():
-    output = r'''
-digraph {
-0 [label="Module" shape=box]
-1 [label="list" shape=box]
-2 [label="Expr" shape=box]
-3 [label="BinOp" shape=box]
-4 [label="Constant\nvalue: 2" shape=box]
-5 [label="Add" shape=box]
-6 [label="Constant\nvalue: 2" shape=box]
+    output = r"""
+digraph G {
+0 [label="Module" shape=box style="rounded, filled"]
+1 [label="list" shape=box style="rounded, filled"]
+2 [label="Expr" shape=box style="rounded, filled"]
+3 [label="BinOp" shape=box style="rounded, filled"]
+4 [label="Constant\nvalue: 2" shape=box style="rounded, filled"]
+5 [label="Add" shape=box style="rounded, filled"]
+6 [label="Constant\nvalue: 2" shape=box style="rounded, filled"]
 0 -> 1 [label=".body"]
 1 -> 2 [label="[0]"]
 2 -> 3 [label=".value"]
@@ -18,5 +18,5 @@ digraph {
 3 -> 5 [label=".op"]
 3 -> 6 [label=".right"]
 }
-    '''
+    """
     assert astdot.source_to_dot('2 + 2', style='').strip() == output.strip()
